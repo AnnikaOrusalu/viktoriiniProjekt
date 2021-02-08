f34cec83b8c733294f71a817677946e35ec39e2d
luklaur <lauri.luke3@gmail.com>
2021-02-08 16:04:25 +0200

    Initial commit

---
+++ src/main/java/ee/tiim/viktoriin/ViktoriinApplication.java
@@ -0,0 +1,13 @@
+package ee.tiim.viktoriin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViktoriinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViktoriinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/tiim/viktoriin/ViktoriinApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.tiim.viktoriin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViktoriinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
