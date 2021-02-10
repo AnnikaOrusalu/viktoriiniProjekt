b411c3a78949e7f6f8f41162d1ef564337fe4980
ErikOlkonen <77267296+ErikOlkonen@users.noreply.github.com>
2021-02-10 15:38:48 +0200

    10.02.2021 QuizController addAnswer method

--- src/main/java/ee/tiim/viktoriin/quiz/QuizController.java
+++ src/main/java/ee/tiim/viktoriin/quiz/QuizController.java
@@ -39,14 +39,29 @@ public class QuizController {
         jdbcTemplate.update(sql, paramMap);
     }
 
+    @PostMapping("addAnswersAndValues")
+    public void addAnswers(@RequestBody JsonController linker) {
+        String sql = "INSERT INTO answers (answer_text, true_false, answers_to_question) VALUES (:answer_text, :true_false, :answers_to_question)";
+        Map<String, Object> paramMap3 = new HashMap();
+        paramMap3.put("answer1", linker.getAnswer1());
+        paramMap3.put("answer2", linker.getAnswer2());
+        paramMap3.put("answer3", linker.getAnswer3());
+        paramMap3.put("answer4", linker.getAnswer4());
+        String answer1value;
+        String answer2value;
+        String answer3value;
+        String answer4value;
+        jdbcTemplate.update(sql, paramMap3);
+    }
 
-   /* @PostMapping("addQuestionAndAnswers")
-    public void addQuestionAndAnswers(@RequestBody JsonThingy linker) {
-        String sql1 = "INSERT INTO questions (question_text) VALUES (:question_text)";
-        String sql2 = "INSERT INTO answers (question_text) VALUES (:question_text)";
-        Map<String, Object> paramMap1 = new HashMap();
-        paramMap.put("question_text", linker.getQuestionText());
-        jdbcTemplate.update(sql, paramMap);
-    }*/
+
+//    @PostMapping("addQuestionAndAnswers")
+//    public void addQuestionAndAnswers(@RequestBody JsonController linker) {
+//        String sql1 = "INSERT INTO questions (question_text) VALUES (:question_text)";
+//        String sql2 = "INSERT INTO answers (question_text) VALUES (:question_text)";
+//        Map<String, Object> paramMap2 = new HashMap();
+//        paramMap2.put("question_text", linker.getQuestionText());
+//        jdbcTemplate.update(sql1, paramMap2);
+//    }
 
 }
